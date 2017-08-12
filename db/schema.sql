57c0a2656b34b00dcaa42ae738321cc4e36ee090
gowrimanoharir <gowrimanohari.r@gmail.com>
2017-08-11 23:18:49 -0500

    created base folder structure

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+create database burgers_db;
+
+use burgers_db;
+
+create table burgers (
+	id integer(10) not null auto_increment,
+    burger_name varchar(100) not null,
+    devoured boolean default false,
+    date timestamp default current_timestamp,
+    primary key (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+insert into burgers_db.burgers (burger_name) values ('Black Bean Chipotle');
+insert into burgers_db.burgers (burger_name) values ('Quinoa Sriracha');
+insert into burgers_db.burgers (burger_name) values ('Chickpea Mango Habanero');
+
