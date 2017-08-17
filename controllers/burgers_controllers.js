eca827276b19961b7df20f9e1a68f2a306b8ce2a
gowrimanoharir <gowrimanohari.r@gmail.com>
2017-08-16 23:22:34 -0500

    completed routing and basic handlebar displays

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,15 +1,35 @@
 const express = require("express");
 const burger = require("../models/burger");
 
-const router = express.router();
+const router = express.Router();
 
 router.get("/", function(req, res) {
-  burger.all(function(data) {
+  burger.selectAll(function(data) {
     var allBrgs = {
       burgers: data
     };
-    console.log(burgers);
-    //res.render("index", hbsObject);
+    //console.log(allBrgs);
+    res.render("index", allBrgs);
+  });
+});
+
+
+router.post("/", function(req, res) {
+  burger.insertOne(req.body.name, function() {
+    res.redirect("/");
+  });
+});
+
+
+router.put("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.updateOne({
+    devoured: req.body.devoured
+  }, condition, function() {
+    res.redirect("/");
   });
 });
 
--- server.js
+++ server.js
@@ -4,3 +4,24 @@ var methodOverride = require("method-override");
 
 var app = express();
 var port = 3000;
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controllers.js");
+
+app.use("/", routes);
+
+app.listen(port);
